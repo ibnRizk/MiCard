9ad1131cac8f8b7cdf0d948c8f4172d9567a0ced
Hussein Rizk <husseinrizk117@gmail.com>
2025-03-20 06:03:25 +0200

    create MiCard screen

--- lib/screens/micard_screen.dart
+++ lib/screens/micard_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
+import '../constant.dart';
+
 class MicardScreen extends StatelessWidget {
   const MicardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Hussein Rizk',
+            style: kTextStyle,
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            'Flutter Developer',
+            style: kTextstyle2,
+          ),
+          Divider(
+            thickness: 1,
+            endIndent: 60,
+            indent: 60,
+          ),
+        ],
+      ),
+    );
   }
 }
