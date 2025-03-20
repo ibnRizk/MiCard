ad828ef8931a0d4eff162af103d866661dfcc601
Hussein Rizk <husseinrizk117@gmail.com>
2025-03-20 06:00:02 +0200

    add constant file

---
+++ lib/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+var kTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 36,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Pacifico');
+const kTextstyle2 =
+    TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w300);
--- lib/screens/micard_screen.dart
+++ lib/screens/micard_screen.dart
@@ -1,46 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../constats.dart';
-import '../widget/custom_list_tile.dart';
-
 class MicardScreen extends StatelessWidget {
   const MicardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 92,
-            backgroundColor: Colors.white,
-            child: CircleAvatar(
-              radius: 91,
-              backgroundImage: AssetImage('assets/photo_hussein.jpg'),
-            ),
-          ),
-          Text(
-            'Hussein Rizk',
-            style: kTextStyle,
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Text(
-            'Flutter Developer',
-            style: kTextstyle2,
-          ),
-          Divider(
-            thickness: 1,
-            endIndent: 60,
-            indent: 60,
-          ),
-          CustomListTile(icon: Icons.phone, text: '+20 01026929650'),
-          CustomListTile(icon: Icons.phone, text: 'husseinrizk117@gmail.com'),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
